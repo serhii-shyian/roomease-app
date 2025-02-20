59aae91dcc6ccc70f0152ae7a41d166a380725b1
Serhii Shyian <serhii.shn@gmail.com>
2025-02-20 17:20:09 +0200

    add user managing feature

--- src/main/java/com/example/roomease/repository/user/UserRepository.java
+++ src/main/java/com/example/roomease/repository/user/UserRepository.java
@@ -2,15 +2,12 @@ package com.example.roomease.repository.user;
 
 import com.example.roomease.model.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long>,
         JpaSpecificationExecutor<User> {
-    @Query("from User u left join fetch u.roles where u.email = :email")
-    Optional<User> findByEmail(String email);
-
-    @Query("from User u left join fetch u.roles where u.username = :username")
+    @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByUsername(String username);
 }
--- src/main/java/com/example/roomease/service/user/UserServiceImpl.java
+++ src/main/java/com/example/roomease/service/user/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.roomease.service.user;
 
 import com.example.roomease.dto.user.UserRegisterRequestDto;
 import com.example.roomease.dto.user.UserResponseDto;
+import com.example.roomease.exception.EntityNotFoundException;
 import com.example.roomease.exception.RegistrationException;
 import com.example.roomease.mapper.UserMapper;
 import com.example.roomease.model.Role;
@@ -11,6 +12,7 @@ import com.example.roomease.repository.user.UserRepository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -36,7 +38,7 @@ public class UserServiceImpl implements UserService {
 
         User userFromDto = userMapper.toEntity(registrationDto);
         userFromDto.setPassword(passwordEncoder.encode(registrationDto.password()));
-        userFromDto.setRoles(findByNameContaining(List.of(
+        userFromDto.setRoles(findByNameContaining(Set.of(
                 Role.RoleName.USER)));
 
         User savedUser = userRepository.save(userFromDto);
@@ -44,7 +46,43 @@ public class UserServiceImpl implements UserService {
         return userMapper.toDto(savedUser);
     }
 
-    private Set<Role> findByNameContaining(List<Role.RoleName> rolesList) {
-        return new HashSet<>(roleRepository.findAllByNameContaining(rolesList));
+    @Override
+    public UserResponseDto updateUserRoles(Long userId, List<String> roleNames) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new EntityNotFoundException("User not found with ID: " + userId));
+
+        Set<Role.RoleName> roleEnums = roleNames.stream()
+                .map(roleName -> Role.RoleName.valueOf(roleName.toUpperCase()))
+                .collect(Collectors.toSet());
+
+        Set<Role> roles = roleRepository.findAllByNameContaining(roleEnums);
+        if (roles.isEmpty()) {
+            throw new EntityNotFoundException("No matching roles found for the provided names.");
+        }
+
+        user.getRoles().clear();
+        user.getRoles().addAll(roles);
+        userRepository.save(user);
+
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public UserResponseDto findProfile(User user) {
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public UserResponseDto updateProfile(User user, UserRegisterRequestDto updateDto) {
+        userMapper.updateEntityFromDto(user, updateDto);
+
+        user.setPassword(passwordEncoder.encode(updateDto.password()));
+        user.setRoles(user.getRoles());
+
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    private Set<Role> findByNameContaining(Set<Role.RoleName> rolesSet) {
+        return new HashSet<>(roleRepository.findAllByNameContaining(rolesSet));
     }
 }
