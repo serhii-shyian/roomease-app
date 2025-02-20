43bb1e2b707eb51f395919ae7f7344bb3d2ad4d6
Serhii Shyian <serhii.shn@gmail.com>
2025-02-20 16:00:12 +0200

    initial commit

---
+++ src/main/java/com/example/roomease/RoomeaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomeaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomeaseAppApplication.class, args);
+    }
+
+}
